eb6f19cceb0a3af5fff3d084a3848d5a825dee0a
Nathan <bunkumcode@gmail.com>
2025-01-09 11:06:59 +0000

    update save

--- audit-form/auditScript.js
+++ audit-form/auditScript.js
@@ -426,43 +426,6 @@ function save_overlay() {
     console.log(comments);
 }
 
-function to_excel() {
-    fetch('questions.json')
-    .then(response => {
-        return response.json(); 
-    })
-    .then(data => {
-        questions = data;
-    });
-
-    var csv = "forms \n" + 
-            "," + JSON.stringify(questions[0]["documentation"]) + "\n Documentational form answers," + documentaional_answers + 
-            "\n," + JSON.stringify(questions[1]["first-aid"]) + "\n first aid/emergency answers ," + emergency_answers +  
-            "\n," + JSON.stringify(questions[2]["welfare"]) + "\n welfare answers ," + welfare_answers + 
-            "\n," + JSON.stringify(questions[3]["COSSH"]) + "\n COSH answers ," + cossh_answers + 
-            "\n," + JSON.stringify(questions[4]["equipment"]) + "\n  equipment answers ," + equipment_answers + 
-            "\n," + JSON.stringify(questions[5]["ppe"]) +"\n PPE answers ," + ppe_answers + 
-            "\n," + JSON.stringify(questions[6]["high-risk"]) + "\n High risk operations ," + hro_answers + 
-            "\n\n photos," + "question type," + "question number," + "\n";
-
-    let iteration = 0;
-    for (i in photos) 
-    {
-        iteration = iteration + 1;
-
-        csv = csv + (photos[i] + ",");
-        if  (iteration === 3) {
-            csv = csv + "\n";
-            iteration = 0;
-        };
-    };
-
-    var hiddenElement = document.createElement('a');
-    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
-    hiddenElement.target = '_blank';
-    hiddenElement.download = 'data.csv';
-    hiddenElement.click();
-}
 
 function to_pdf() {
     window.location.replace('PDFpreview.html');
@@ -490,7 +453,6 @@ function back_to_questions() {
 }
 
 function loadPDF() {
-    
     var PDFcontainer = document.getElementById("PDFpreview-container-1");
     PDFcontainer.innerHTML +=  `<h2 style="text-align:center">Site Audit Completion</h2>
                                 <br>
@@ -600,7 +562,6 @@ function loadQuestions(category, page, jsonindex, amountQuest, title, start) {
                     
                 };
      
-                console.log(pdf_comments)
                 if (category == pdf_comments[j+2] && i+1 == pdf_comments[j+1]) {
                     var comment = pdf_comments[j];
                 } else {
@@ -659,57 +620,70 @@ function loadphotos() {
                                         <img class="imgPDF" src= ${pdf_photos[x]} ></img> <br>
                                         </div>
                                     `;
-
-        
+     
     };
-
-    
-
 }
 
-async function PDFcalculate() {
+function PDFcalculate() {
     const pdf_photos = JSON.parse(localStorage.getItem("local_photos"));
     localStorage.clear();
 
+    
+    fetch('questions.json')
+            .then(response => response.json())
+            .then(data => {
 
+                const { jsPDF } = window.jspdf;
+                const doc = new jsPDF();
+                
+                
+                for (let i = start, j = 0; i < amountQuest, j < amountQuest*3; i++, j = j + 3) {
+                    alert()
+                    if (jsonindex === 0) {
+                        var answers  = pdf_documentaional_answers;
+                    } else if (jsonindex === 1) {
+                        var answers  = pdf_emergency_answers;
+                    } else if (jsonindex === 2) {
+                        var answers  = pdf_welfare_answers;
+                    } else if (jsonindex === 3) {
+                        var answers  = pdf_cossh_answers;
+                    } else if (jsonindex === 4) {
+                        var answers  = pdf_equipment_answers;
+                    } else if (jsonindex === 5) {
+                        var answers  = pdf_ppe_answers;
+                    } else if (jsonindex === 6) {
+                        var answers  = pdf_hro_answers;
+                    };
 
-    document.body.style.zoom = "100%";
-    const { jsPDF } = window.jspdf;
-    const pdf = new jsPDF('p', 'mm', 'a4'); // Portrait orientation, mm units, A4 size
-
-    const margin = 0.5; // 1mm margin
-
-    // Function to capture a div and add it to the PDF
-    function addDivToPDF(divId, isFirst) {
-        return new Promise((resolve) => {
-            html2canvas(document.querySelector(`#${divId}`)).then(canvas => {
-                const imgData = canvas.toDataURL("image/png");
-                const imgWidth = 210 - 2 * margin; // A4 width in mm minus margins
-                const imgHeight = (canvas.height * imgWidth) / canvas.width; // Maintain aspect ratio
-
-                // Add a new page for each div, but not for the first one
-                if (!isFirst) {
-                    pdf.addPage(); // Add a new page for subsequent images
+                    let question = data[jsonindex][category][i]["question"];
+    
+                    if (i == 0) {
+                        doc.text(`${title}`, 10, 10);
+                        if (category == "high-risk" && start ==  0){
+                            doc.text("Breaking Ground");
+                        };
+                        
+                    } else if (i === 6 && category == "high-risk") {
+                        doc.text(`${title}`, 10, 10);
+                        if (category == "high-risk" && start ==  6){
+                            doc.text("Working at Height");
+                        };
+                        
+                    } else if (i === 11 && category == "high-risk" &&  start == 11) {
+                        doc.text(`${title}`, 10, 10);
+                        if (category == "high-risk" && start ==  11){
+                            doc.text("Confined Spaces");
+                        };
+                        
+                    } else if (i === 18 && category == "high-risk" && start ==  18) {
+                        doc.text(`${title}`, 10, 10);
+                        if (category == "high-risk" && start ==  18){
+                            doc.text("Lifting");
+                        };
+                    };
                 }
-
-                // Add the image to the PDF with the specified margin
-                pdf.addImage(imgData, 'PNG', margin, margin, imgWidth, imgHeight); // Position with margin
-                resolve();
-            });
+                doc.save("a4.pdf");
         });
-    }
-
-    // Process each div sequentially
-    const promises = divIds.reduce((promiseChain, divId, index) => {
-        return promiseChain.then(() => addDivToPDF(divId, index === 0));
-    }, Promise.resolve());
-
-    promises.then(() => {
-        pdf.save("download.pdf");
-        downloadComplete();
-    }).catch(error => {
-        console.error("Error generating PDF:", error);
-    });
 }
 
 async function downloadSplash() {
